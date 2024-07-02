80ea6e2dc4a3e4658af1b288f0ce8b9b57c4d0a4
Paula Fenner <fenner.paula@gmail.com>
2024-07-02 18:25:49 +0200

    Added functionality on JSfile

---
+++ review.md
@@ -0,0 +1,34 @@
+# What you've done well
+
+- the app works very well, and you made it naturally responsive
+- using very important meta tags like doctype, lang="en", title, viewport
+- semantic HTML <main> <section>, logical order of headings
+- everything is nicely indented
+- useful comments in index.js
+- extracting value from an input
+- use of .toFixed(3)
+- use of template strings `${}`
+- use of classes is consistent. Know how to use type selectors like button or h3
+
+# What could be improved
+
+- JS
+  [x] let v. const
+  [x] refactoring for improved readability - concepts used:
+
+  - separation of concerns
+  - parameters and arguments
+  - locally-scoped variables
+
+- design:
+  [x] adding a bit of padding on main to give it more breathing room
+  [x] adding a min-height to .conversion-container to avoid the layout shift when renderinig results
+
+- HTML
+  [x] be aware that strings like "Metric/Imperial" with no space are treated as ONE word and will nt break when there's no more space on the line
+  [x] <input
+            type="text"
+            id="input-el"
+            placeholder="0" />
+  Are you aware of ther types of inputs? Here, we could use type="number"
+  For your curiosity: https://www.w3schools.com/html/html_form_input_types.asp
--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const inputEl = document.getElementById('input-el');
+const convertBtn = document.getElementById('convert-btn');
+
+const baseValue = inputEl.value;
+
+convertBtn.addEventListener('click', render);
+
+function render() {
+  getValue();
+  clearInput();
+}
+
+function getValue() {
+  convertLenght(baseValue);
+  convertVolume(baseValue);
+  convertMass(baseValue);
+}
+
+function clearInput() {
+  inputEl.value = '';
+}
+
+function convertLenght(baseValue) {
+  const meter = 3.281;
+  const lengthEl = document.getElementById('length-el');
+  lengthEl.textContent += `
+    ${baseValue} meters = ${(baseValue * meter).toFixed(3)} feet | 
+    ${baseValue} feet = ${(baseValue / meter).toFixed(3)} meters
+    `;
+}
+
+function convertVolume(baseValue) {
+  const liter = 0.264;
+  const volumeEl = document.getElementById('volume-el');
+  volumeEl.textContent += `
+    ${baseValue} meters = ${(baseValue * liter).toFixed(3)} feet | 
+    ${baseValue} feet = ${(baseValue / liter).toFixed(3)} meters
+    `;
+}
+
+function convertMass(baseValue) {
+  const kilo = 2.204;
+  const massEl = document.getElementById('mass-el');
+  massEl.textContent += `
+    ${baseValue} meters = ${(baseValue * kilo).toFixed(3)} feet | 
+    ${baseValue} feet = ${(baseValue / kilo).toFixed(3)} meters
+    `;
+}
+
+/* CHALLENGE
+function convert(baseValue, conversionType, unit1, unit2, conversionFactor) {
+    const result1 = `${baseValue} ${unit1} = ${(baseValue * conversionFactor).toFixed(3)} ${unit2}`;
+    const result2 = `${baseValue} ${unit2} = ${(baseValue / conversionFactor).toFixed(3)} ${unit1}`;
+  
+    return `${result1} | ${result2} (${conversionType})`;
+  } */
